eaba4b22c503764b3a44d68e6b5af8b12fff5f82
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2024-04-27 01:22:15 +0200

    Update toLowercase.js

--- toLowercase.js
+++ toLowercase.js
@@ -1,129 +1,148 @@
-window.JFWidgetLowercaseSetup ||
-    (window.JFWidgetLowercaseSetup = function (i) {
-        console.log("test")
-        if (((this.served = void 0 !== this.served ? this.served : []), ~this.served.indexOf(i.qid))) return !1;
-        this.served.push(i.qid);
-        e = navigator.userAgent.toLowerCase();
-        var e,
-            u =
-                /iphone|ipod|android|blackberry|opera mini|opera mobi|skyfire|maemo|windows phone|palm|iemobile|symbian|symbianos|fennec/i.test(e) ||
-                /ipad|android 3|sch-i800|playbook|tablet|kindle|gt-p1000|sgh-t849|shw-m180s|a510|a511|a100|dell streak|silk/i.test(e) ||
-                (-1 < e.indexOf("macintosh") && 1 < navigator.maxTouchPoints);
-        function n(n) {
-            console.log("test")
-            var a = "input_" + n.qid,
-                t = parseInt(n.start) || 0,
-                i = parseInt(n.end) || 0;
-            function d() {
-                return n.source.replace(/^#/i, "");
-            }
-            function o(e, t, i) {
-                i
-                    ? $(e).observe(t, function () {
-                        setTimeout(s, 1e3);
-                    })
-                    : setTimeout(function () {
-                        $(e).observe(t, s);
-                    }, 50);
-            }
-            function s() {
-                var e = document.getElementById("input_" + r(d())) || $(d()),
-                    e = (null != e ? e.value : JotForm.fieldHasContent(r(d())) || "").toLowerCase();
-                $(a).setValue(e), document.getElementById(a).triggerEvent("change"), JotForm.runConditionForId(n.qid);
-            }
-            function r(e) {
-                console.log("test")
-                return (e = e.split("_")).length ? e[1] : "";
-            }
-            this.init = function () {
-                console.log("test")
-                var e,
-                    t,
-                    i = $(d());
-                i
-                    ? ((t = new Element("input", { type: "text", id: a, name: n.qname, readonly: "readonly", className: "form-textbox to-lowercase" }).setStyle({ "box-sizing": "border-box" })),
-                        $(a + "_container").insert(t),
-                        (t = $$(".to-lowercase#" + a).first()),
-                        (e = t.parentNode.parentNode.style.width),
-                        t.setStyle({ width: e }),
-                        t.up('div[id*="cid"]').setStyle({ display: "inline-block" }),
-                        (function (e) {
-                            var t = r(e.id),
-                                e = JotForm.getInputType(t),
-                                i = $("id_" + t);
-                            switch (e) {
-                                case "radio":
-                                case "checkbox":
-                                case "signature":
-                                    o(i, "click");
-                                    break;
-                                case "select":
-                                    o(i, "change");
-                                    break;
-                                case "file":
-                                    o(i, "change"),
-                                        "multiple" ===
-                                        $$("#id_" + t + " input")
-                                            .first()
-                                            .readAttribute("multiple") && o(i, "click", !0);
-                                    break;
-                                case "datetime":
-                                    o(i, "date:changed"),
-                                        $$("#id_" + t + " select").each(function (e) {
-                                            o($(e), "change");
-                                        });
-                                    break;
-                                case "time":
-                                    $$("#id_" + t + " select").each(function (e) {
-                                        o($(e), "change");
-                                    }),
-                                        $$("#id_" + t + " input").each(function (e) {
-                                            null !== $(e).readAttribute("data-mask") && o($(e), "change");
-                                        });
-                                    break;
-                                case "birthdate":
-                                    $$("#id_" + t + " select").each(function (e) {
-                                        o($(e), "change");
-                                    });
-                                    break;
-                                case "address":
-                                case "email":
-                                    o(i, "keyup"), o(i, "change");
-                                    break;
-                                case "number":
-                                    o(i, "keyup"), o(i, "click"), o(i, "change");
-                                    break;
-                                case "text":
-                                    $("input_" + t).readAttribute("data-masked") && u ? o(i, "keyup", !0) : (o(i, "keyup"), o(i, "change"));
-                                    break;
-                                case "widget":
-                                    o($("input_" + t), "change"), JotForm.widgetsWithConditions.push(t);
-                                    break;
-                                default:
-                                    o(i, "keyup"), o(i, "change");
-                            }
-                        })(i),
-                        setTimeout(function () {
-                            JotForm.runConditionForId(n.qid);
-                        }, 50),
-                        (e = i && i.id && i.id in JotForm.defaultValues),
-                        (JotForm.isEditMode() || e) &&
-                        setTimeout(function () {
-                            i.value && s();
-                        }, 1e3))
-                    : ((t = new Element("p").setStyle("color", "#F00").update("Invalid source field ID")), $(a + "_container").insert(t));
-            };
+window.JFWidgetLowercaseSetup = function(request) {
+    function init(file) {
+      function error() {
+        return file.source.replace(/^#/i, "");
+      }
+      function trigger(element, event, dataAndEvents) {
+        if (dataAndEvents) {
+          $(element).observe(event, function() {
+            setTimeout(check, 1E3);
+          });
+        } else {
+          setTimeout(function() {
+            $(element).observe(event, check);
+          }, 50);
         }
-        console.log("test")
-        document.write('<div id="input_' + i.qid + '_container"></div>'),
-            document.observe("dom:loaded", function () {
-                var e = new n(i),
-                    t = setInterval(() => {
-                        JotForm.initializing || (e.init(), clearInterval(t));
-                    }, 100);
-            });
-    }),
-    void 0 !== window.srcFieldId &&
-    void 0 !== window.lowercaseQid &&
-    void 0 !== window.lowercaseQname &&
-    window.JFWidgetLowercaseSetup({ source: window.srcFieldId, qid: window.lowercaseQid, qname: window.lowercaseQname });
+      }
+      function check() {
+        var name = document.getElementById("input_" + cb(error())) || $(error());
+        name = (null != name ? name.value : JotForm.fieldHasContent(cb(error())) || "").toLowerCase();
+        $(id).setValue(name);
+        document.getElementById(id).triggerEvent("change");
+        JotForm.runConditionForId(file.qid);
+      }
+      function cb(result) {
+        return(result = result.split("_")).length ? result[1] : "";
+      }
+      var id = "input_" + file.qid;
+      var nameStart = parseInt(file.start) || 0;
+      var indexOfEquals = parseInt(file.end) || 0;
+      this.init = function() {
+        var originalWidth;
+        var element;
+        var elem = $(error());
+        if (elem) {
+          element = (new Element("input", {
+            type : "text",
+            id : id,
+            name : file.qname,
+            readonly : "readonly",
+            className : "form-textbox toLowercase"
+          })).setStyle({
+            "box-sizing" : "border-box"
+          });
+          $(id + "_container").insert(element);
+          element = $$(".toLowercase#" + id).first();
+          originalWidth = element.parentNode.parentNode.style.width;
+          element.setStyle({
+            width : originalWidth
+          });
+          element.up('div[id*="cid"]').setStyle({
+            display : "inline-block"
+          });
+          (function(type) {
+            var name = cb(type.id);
+            type = JotForm.getInputType(name);
+            var submit = $("id_" + name);
+            switch(type) {
+              case "radio":
+              ;
+              case "checkbox":
+              ;
+              case "signature":
+                trigger(submit, "click");
+                break;
+              case "select":
+                trigger(submit, "change");
+                break;
+              case "file":
+                trigger(submit, "change");
+                if ("multiple" === $$("#id_" + name + " input").first().readAttribute("multiple")) {
+                  trigger(submit, "click", true);
+                }
+                break;
+              case "datetime":
+                trigger(submit, "date:changed");
+                $$("#id_" + name + " select").each(function(mediaElem) {
+                  trigger($(mediaElem), "change");
+                });
+                break;
+              case "time":
+                $$("#id_" + name + " select").each(function(mediaElem) {
+                  trigger($(mediaElem), "change");
+                });
+                $$("#id_" + name + " input").each(function(element) {
+                  if (null !== $(element).readAttribute("data-mask")) {
+                    trigger($(element), "change");
+                  }
+                });
+                break;
+              case "birthdate":
+                $$("#id_" + name + " select").each(function(mediaElem) {
+                  trigger($(mediaElem), "change");
+                });
+                break;
+              case "address":
+              ;
+              case "email":
+                trigger(submit, "keyup");
+                trigger(submit, "change");
+                break;
+              case "number":
+                trigger(submit, "keyup");
+                trigger(submit, "click");
+                trigger(submit, "change");
+                break;
+              case "text":
+                if ($("input_" + name).readAttribute("data-masked") && data_masked) {
+                  trigger(submit, "keyup", true);
+                } else {
+                  trigger(submit, "keyup");
+                  trigger(submit, "change");
+                }
+                break;
+              case "widget":
+                trigger($("input_" + name), "change");
+                JotForm.widgetsWithConditions.push(name);
+                break;
+              default:
+                trigger(submit, "keyup");
+                trigger(submit, "change");
+            }
+          })(elem);
+          setTimeout(function() {
+            JotForm.runConditionForId(file.qid);
+          }, 50);
+          originalWidth = elem && (elem.id && elem.id in JotForm.defaultValues);
+          if (JotForm.isEditMode() || originalWidth) {
+            setTimeout(function() {
+              if (elem.value) {
+                check();
+              }
+            }, 1E3);
+          }
+        } else {
+          element = (new Element("p")).setStyle("color", "#F00").update("Invalid source field ID");
+          $(id + "_container").insert(element);
+        }
+      };
+    }
+    if (this.served = void 0 !== this.served ? this.served : [], ~this.served.indexOf(request.qid)) {
+      return false;
+    }
+    this.served.push(request.qid);
+    agent = navigator.userAgent.toLowerCase();
+    var agent;
+    var data_masked = /iphone|ipod|android|blackberry|opera mini|opera mobi|skyfire|maemo|windows phone|palm|iemobile|symbian|symbianos|fennec/i.test(agent) || (/ipad|android 3|sch-i800|playbook|tablet|kindle|gt-p1000|sgh-t849|shw-m180s|a510|a511|a100|dell streak|silk/i.test(agent) || -1 < agent.indexOf("macintosh") && 1 < navigator.maxTouchPoints);
+  };
+  
