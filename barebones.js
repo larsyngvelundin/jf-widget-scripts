af85e02417e03e923c6c69a1ec24decadd8455f4
Lars <34173819+larsyngvelundin@users.noreply.github.com>
2024-06-26 08:08:56 +0200

    Create barebones test

---
+++ barebones.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function() {
+    JFCustomWidget.subscribe("ready", function(e) {
+        console.log("ready");
+        console.log("JFCustomWidget.getWidgetSettings:");
+        console.log(JFCustomWidget.getWidgetSettings());
+    }
+});
